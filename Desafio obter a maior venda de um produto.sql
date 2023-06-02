fbea3d0161bb93c58220eff7ab7ec68950a1c3e0
Amanda Ornelas <amanda.ornelas@confitec.com.br>
2023-06-02 09:04:50 -0300

    unico commit

---
+++ Desafio obter a maior venda de um produto.sql
@@ -0,0 +1,6 @@
+SELECT * FROM TABELA_DE_PRODUTOS 
+WHERE nome_do_produto = 'Linha Refrescante - 1 Litro - Morango/Limao';
+
+SELECT * FROM ITENS_NOTAS_FISCAIS
+WHERE codigo_do_produto = '1101035'
+ORDER BY QUANTIDADE DESC;
